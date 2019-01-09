b1467330e6b1635453b1d0ceb6e6f8527736c0fa
大花 <tamen2009@163.com>
2019-01-09 10:01:56 +0800

    new02

--- lib/getQRCode.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('./phpqrcode.php');
-if(isset($_GET["url"])) {
-    $url = $_GET["url"];
-    QRcode::png($url, false, 'H', '6', '1');
-}
-else {
-    echo "<script>window.location.href = '//' + window.location.hostname;</script>";
-}
